cc94d743443fc7f2959ecbb9b6179623c3bef9a9
Jay Beavers <Jay.Beavers@Microsoft.Com>
2012-06-28 16:58:19 -0700

    Light update to frame buffer allocator in reflect frames

--- ReflectaFramesSerial/ReflectaFramesSerial.h
+++ ReflectaFramesSerial/ReflectaFramesSerial.h
@@ -22,7 +22,8 @@ namespace reflectaFrames
 {
 	// Function definition for Frame Buffer Allocation function, to be optionally implemented by
 	// the calling library or application.
-	typedef void (*frameBufferAllocationFunction)(byte** frameBuffer, byte* frameBufferLength);
+	// Returns the size of the frame buffer
+	typedef byte (*frameBufferAllocationFunction)(byte** frameBuffer);
 	
 	// Function definition for the Frame Received function.
 	typedef void (*frameReceivedFunction)(byte sequence, byte frameLength, byte* frame);
