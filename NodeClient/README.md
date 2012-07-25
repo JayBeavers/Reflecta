7645c44cdb9ca2e153411a11c4b10dae12e69454
Jay Beavers <jay.beavers@microsoft.com>
2012-07-25 09:09:53 -0700

    Initial placeholder files for turning node.js reflecta client into a 'real' module

---
+++ NodeClient/README.md
@@ -0,0 +1,4 @@
+Overview
+========
+
+Reflecta is a node.js client for communicating with an Arduino via the Reflecta protocol.
---
+++ NodeClient/test/basics.js
@@ -0,0 +1,7 @@
+var assert = require('chai').assert;
+
+suite('Hello', function() {
+    test('Hello test', function() {
+        assert(true, 'Worked!');
+    });
+});
