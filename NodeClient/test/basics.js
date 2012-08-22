15073c9f1fb1df1007ea50f7b44eb91eb0d25921
Jay Beavers <jay.beavers@microsoft.com>
2012-08-22 15:57:52 -0700

    Cleaning up of tests, moving heartbeat tests into separate section

--- NodeClient/test/basics.js
+++ NodeClient/test/basics.js
@@ -1,41 +1,39 @@
 /*global describe */
 /*global it */
 
-var devicePath = "/dev/ttyACM0";
+var devicePath = "COM4";
 
 var assert = require('chai').assert;
 var util = require('util');
 
-describe('Hello', function() {
+describe('Basic Reflexes', function() {
     
-  it('Reflecta', function(done) {
+  it('QueryInterface Responds', function(done) {
       
     var Reflecta = require('../reflecta.js');
     var reflecta = new Reflecta(devicePath, function(err) {
       if (err) {
         reflecta.close(function() { done(err); });
-        return;
       }
 
       reflecta.on('response', function(response) {
-        // console.log(response.sequence + " : " + response.message);
         reflecta.close(done);
       });
       
-      reflecta.sendFrame([1]);
+      reflecta.sendFrame(reflecta.FunctionIds.queryInterface);
     });
     
     reflecta.on('error', function(err) { console.log(err); });
+    reflecta.on('warning', function(warning) { console.log(warning); });
     reflecta.on('portError', function(err) { console.log(err); });
 
     reflecta.on('message', function(message) { console.log(message); });
-    reflecta.on('frame', function(frame) { console.log(frame.sequence + " : " + frame.data); });
     reflecta.on('portClose', function() { console.log('portClose'); });
     reflecta.on('portEnd', function() { console.log('portEnd'); });
     reflecta.on('portOpen', function() { console.log('portOpen'); });
   });
   
-  it('Blinky', function(done) {
+  it('ARDU1 Blinky works', function(done) {
 
       var Reflecta = require('../reflecta.js');
       var reflecta = new Reflecta(devicePath, function(err) {
@@ -44,18 +42,128 @@ describe('Hello', function() {
           return;
         }
 
-        reflecta.on('error', function(err) { console.log(err); });
-        reflecta.on('portError', function(err) { console.log(err); });
-        
-        setInterval(function() { reflecta.ardu1.gpio.digitalWrite(11, 1); }, 199);
-        setInterval(function() { reflecta.ardu1.gpio.digitalWrite(11, 0); }, 400);
+        setInterval(function() { reflecta.ARDU1.gpio.digitalWrite(11, 1); }, 199);
+        setInterval(function() { reflecta.ARDU1.gpio.digitalWrite(11, 0); }, 400);
         
         setTimeout(function() { reflecta.close(done); }, 1700);
       });
+
+    reflecta.on('error', function(err) { console.log(err); });
+    reflecta.on('warning', function(warning) { console.log(warning); });
+    reflecta.on('portError', function(err) { console.log(err); });
+
+    reflecta.on('message', function(message) { console.log(message); });
+    reflecta.on('portClose', function() { console.log('portClose'); });
+    reflecta.on('portEnd', function() { console.log('portEnd'); });
+    reflecta.on('portOpen', function() { console.log('portOpen'); });
   });
-  
+
+  it('PushArray and SendResponseCount properly round trip', function(done) {
+    var Reflecta = require('../reflecta.js');
+    var reflecta = new Reflecta(devicePath, function(err) {
+      if (err) {
+        reflecta.close(function() { done(err); });
+        return;
+      }
+
+      var w0 = 250;
+      var w1 = -97;
+      var w2 = 650;
+      var w3 = -129;
+
+      var buffer = new Buffer(10);
+      buffer[0] = reflecta.FunctionIds.pushArray;
+      buffer[1] = 8;
+      buffer.writeInt16BE(w0, 2);
+      buffer.writeInt16BE(w1, 4);
+      buffer.writeInt16BE(w2, 6);
+      buffer.writeInt16BE(w3, 8);
+
+      reflecta.sendFrame(buffer);
+
+      reflecta.sendResponseCount(8, function(error, buffer) {
+        if (error) {
+          reflecta.close(function() { done(error); });
+          return;
+        }
+
+        var w00 = buffer.readInt16BE(0);
+        var w01 = buffer.readInt16BE(2);
+        var w02 = buffer.readInt16BE(4);
+        var w03 = buffer.readInt16BE(6);
+
+        assert.equal(w0, w00);
+        assert.equal(w1, w01);
+        assert.equal(w2, w02);
+        assert.equal(w3, w03);
+
+        reflecta.close(function() { done(err); });
+      });
+    });
+
+    reflecta.on('error', function(err) { console.log(err); });
+    reflecta.on('warning', function(warning) { console.log(warning); });
+    reflecta.on('portError', function(err) { console.log(err); });
+
+    reflecta.on('message', function(message) { console.log(message); });
+    reflecta.on('portClose', function() { console.log('portClose'); });
+    reflecta.on('portEnd', function() { console.log('portEnd'); });
+    reflecta.on('portOpen', function() { console.log('portOpen'); });
+
+  });
+
+  it('Simple PushArray and SendResponse properly round trip', function(done) {
+    var Reflecta = require('../reflecta.js');
+    var reflecta = new Reflecta(devicePath, function(err) {
+      if (err) {
+        reflecta.close(function() { done(err); });
+        return;
+      }
+
+      reflecta.on('error', function(err, frame, checksum) { console.log(err); });        
+      reflecta.on('message', function(message) { console.log(message); });
+
+      reflecta.sendFrame([reflecta.FunctionIds.pushArray, 1, 99]);
+
+      reflecta.sendResponse(function(error, buffer) {
+        assert.equal(buffer[0], 99);
+        assert.equal(buffer.length, 1);
+
+        reflecta.sendFrame([reflecta.FunctionIds.pushArray, 1, 98]);
+
+        reflecta.sendResponseCount(1, function(error, buffer) {
+          assert.equal(buffer[0], 98);
+          assert.equal(buffer.length, 1);
+
+          reflecta.sendFrame([reflecta.FunctionIds.pushArray, 2, 98, 99]);
+
+          reflecta.sendResponseCount(2, function(error, buffer) {
+            assert.equal(buffer[0], 98);
+            assert.equal(buffer[1], 99);
+            assert.equal(buffer.length, 2);
+            
+            reflecta.close(function() { done(err); });
+          });
+        });
+      });
+    });
+
+    reflecta.on('error', function(err) { console.log(err); });
+    reflecta.on('warning', function(warning) { console.log(warning); });
+    reflecta.on('portError', function(err) { console.log(err); });
+
+    reflecta.on('message', function(message) { console.log(message); });
+    reflecta.on('portClose', function() { console.log('portClose'); });
+    reflecta.on('portEnd', function() { console.log('portEnd'); });
+    reflecta.on('portOpen', function() { console.log('portOpen'); });
+  });
+});
+
+describe('Reflecta Heartbeat', function() {
+
   // Meant to be run when Reflecta has the ReflectaHeartbeat turned on and running
-  it('BasicHeartbeat', function(done) {
+  // Not a useful test as it doesn't ensure a heartbeat is running and it doesn't check for a result
+  it('Basic Heartbeat detected', function(done) {
 
     var Reflecta = require('../reflecta.js');
     var reflecta = new Reflecta(devicePath, function(err) {
@@ -75,13 +183,13 @@ describe('Hello', function() {
   });
 
   // Meant to be run as the client for BasicIMU
-  it('HeartbeatIMU', function(done) {
+  // Not a useful test as it doesn't ensure a heartbeat is running and it doesn't check for a result
+  it('IMU Heartbeat Received', function(done) {
 
     var Reflecta = require('../reflecta.js');
     var reflecta = new Reflecta(devicePath, function(err) {
       if (err) {
         reflecta.close(function() { done(err); });
-        return;
       }
 
       reflecta.on('error', function(err, frame, checksum) { console.log(err + ' - ' + util.inspect(frame) + ' - ' + checksum); });
@@ -115,89 +223,4 @@ describe('Hello', function() {
       setTimeout(function() { reflecta.close(done); }, 1700);
     });
   });
-
-  it('RoundTripData', function(done) {
-    var Reflecta = require('../reflecta.js');
-    var reflecta = new Reflecta(devicePath, function(err) {
-      if (err) {
-        reflecta.close(function() { done(err); });
-        return;
-      }
-
-      reflecta.on('error', function(err, frame, checksum) { console.log(err + ' - ' + util.inspect(frame) + ' - ' + checksum); });        
-      reflecta.on('message', function(message) { console.log(message); });
-
-      var w0 = 250;
-      var w1 = -97;
-      var w2 = 650;
-      var w3 = -129;
-
-      var buffer = new Buffer(10);
-      buffer[0] = reflecta.FunctionIds.pushArray;
-      buffer[1] = 8;
-      buffer.writeInt16BE(w0, 2);
-      buffer.writeInt16BE(w1, 4);
-      buffer.writeInt16BE(w2, 6);
-      buffer.writeInt16BE(w3, 8);
-
-      reflecta.sendFrame(buffer);
-
-      reflecta.sendResponseCount(8, function(error, buffer) {
-        if (error) {
-          reflecta.close(function() { done(error); });
-          return;
-        }
-
-        var w00 = buffer.readInt16BE(0);
-        var w01 = buffer.readInt16BE(2);
-        var w02 = buffer.readInt16BE(4);
-        var w03 = buffer.readInt16BE(6);
-
-        assert.equal(w0, w00);
-        assert.equal(w1, w01);
-        assert.equal(w2, w02);
-        assert.equal(w3, w03);
-
-        done();
-      });
-    });
-  });
-
-  it('SimplePushResponse', function(done) {
-    var Reflecta = require('../reflecta.js');
-    var reflecta = new Reflecta(devicePath, function(err) {
-      if (err) {
-        reflecta.close(function() { done(err); });
-        return;
-      }
-
-      reflecta.on('error', function(err, frame, checksum) { console.log(err); });        
-      reflecta.on('message', function(message) { console.log(message); });
-
-      reflecta.sendFrame([reflecta.FunctionIds.pushArray, 1, 99]);
-
-      reflecta.sendResponse(function(error, buffer) {
-        assert.equal(buffer[0], 99);
-        assert.equal(buffer.length, 1);
-
-        reflecta.sendFrame([reflecta.FunctionIds.pushArray, 1, 98]);
-
-        reflecta.sendResponseCount(1, function(error, buffer) {
-          assert.equal(buffer[0], 98);
-          assert.equal(buffer.length, 1);
-
-          reflecta.sendFrame([reflecta.FunctionIds.pushArray, 2, 98, 99]);
-
-          reflecta.sendResponseCount(2, function(error, buffer) {
-            assert.equal(buffer[0], 98);
-            assert.equal(buffer[1], 99);
-            assert.equal(buffer.length, 2);
-            done();
-          });
-
-        });
-      });
-    });
-  });
-
 });
