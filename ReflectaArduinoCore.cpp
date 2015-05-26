978bb48cd4b930387d42c8c9f76a64b11f671efa
Jay Beavers <jay@hikinghomeschoolers.org>
2015-05-26 10:21:30 -0700

    Fix behavior changes in compiler that broke functionality in Arduino 1.5.x+

--- ReflectaArduinoCore.cpp
+++ ReflectaArduinoCore.cpp
@@ -11,49 +11,61 @@ namespace reflectaArduinoCore
 {
   void pinMode()
   {
-    ::pinMode(pop(), pop());
+    int8_t pin = pop();
+    int8_t val = pop();
+    ::pinMode(pin, val);
   }
-  
+
   void digitalRead()
   {
-    push(::digitalRead(pop()));
+    int8_t pin = pop();
+    push(::digitalRead(pin));
   }
-  
+
   void digitalWrite()
   {
-    ::digitalWrite(pop(), pop());
+    int8_t pin = pop();
+    int8_t val = pop();
+    ::digitalWrite(pin, val);
   }
-  
+
   void analogRead()
   {
-    push16(::analogRead(pop()));
+    int8_t pin = pop();
+    push16(::analogRead(pin));
   }
-  
+
   void analogWrite()
   {
-    ::analogWrite(pop(), pop());
+    int8_t pin = pop();
+    int8_t val = pop();
+    ::analogWrite(pin, val);
   }
-  
+
   void wireBeginMaster()
   {
     Wire.begin();
   }
-  
+
   void wireRequestFrom()
   {
-    Wire.requestFrom(pop(), pop());
+    int8_t address = pop();
+    int8_t quantity = pop();
+    Wire.requestFrom(address, quantity);
   }
-  
+
   void wireRequestFromStart()
   {
-    Wire.requestFrom(pop(), pop(), false);
+    int8_t address = pop();
+    int8_t quantity = pop();
+    Wire.requestFrom(address, quantity, false);
   }
-  
+
   void wireAvailable()
   {
     push(Wire.available());
   }
-  
+
   void wireRead()
   {
     if (Wire.available())
@@ -61,23 +73,25 @@ namespace reflectaArduinoCore
     else
       reflectaFrames::sendEvent(Error, WireNotAvailable);
   }
-  
+
   void wireBeginTransmission()
   {
-    Wire.beginTransmission(pop());
+    int8_t address = pop();
+        Wire.beginTransmission(address);
   }
-  
+
   // TODO: Support variants write(string) and write(data, length)
   void wireWrite()
   {
-    Wire.write(pop());
+    int8_t val = pop();
+    Wire.write(val);
   }
-  
+
   void wireEndTransmission()
   {
     Wire.endTransmission();
   }
-  
+
   Servo servos[MAX_SERVOS];
 
   // TODO: Support variant attach(pin, min, max)
@@ -89,26 +103,33 @@ namespace reflectaArduinoCore
 
   void servoDetach()
   {
-      servos[pop()].detach();
+    int8_t pin = pop();
+      servos[pin].detach();
   }
 
   void servoWrite()
   {
-      servos[pop()].write(pop());
+    int8_t pin = pop();
+    int8_t val = pop();
+      servos[pin].write(val);
   }
-  
+
   void servoWriteMicroseconds()
   {
-      servos[pop()].writeMicroseconds(pop16());
+    int8_t pin = pop();
+    int8_t val = pop16();
+      servos[pin].writeMicroseconds(val);
   }
-  
+
   // TODO: Support variant pulseIn(pin, value, timeout)
   void pulseIn()
   {
     // BUGBUG: Broken, returns a 32 bit result
-    push(::pulseIn(pop(), pop()));
+    int8_t pin = pop();
+    int8_t val = pop();
+    push(::pulseIn(pin, val));
   }
-  
+
   // Bind the Arduino core methods to the ardu1 interface
   void setup()
   {
@@ -119,16 +140,16 @@ namespace reflectaArduinoCore
     reflectaFunctions::bind("ardu1", analogWrite);
 
     reflectaFunctions::bind("ardu1", wireBeginMaster);
-    
+
     reflectaFunctions::bind("ardu1", wireRequestFrom);
     reflectaFunctions::bind("ardu1", wireRequestFromStart);
     reflectaFunctions::bind("ardu1", wireAvailable);
     reflectaFunctions::bind("ardu1", wireRead);
-    
+
     reflectaFunctions::bind("ardu1", wireBeginTransmission);
     reflectaFunctions::bind("ardu1", wireWrite);
     reflectaFunctions::bind("ardu1", wireEndTransmission);
-    
+
     reflectaFunctions::bind("ardu1", servoAttach);
     reflectaFunctions::bind("ardu1", servoDetach);
     reflectaFunctions::bind("ardu1", servoWrite);
